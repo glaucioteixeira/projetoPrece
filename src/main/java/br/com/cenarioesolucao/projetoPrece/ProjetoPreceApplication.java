e9b353c700a22b912914b1802df6b90d71da63e4
Glaucio <glaucio.teixeira@outlook.com>
2018-10-19 15:57:11 -0300

    Projeto criado

---
+++ src/main/java/br/com/cenarioesolucao/projetoPrece/ProjetoPreceApplication.java
@@ -0,0 +1,12 @@
+package br.com.cenarioesolucao.projetoPrece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPreceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPreceApplication.class, args);
+	}
+}
