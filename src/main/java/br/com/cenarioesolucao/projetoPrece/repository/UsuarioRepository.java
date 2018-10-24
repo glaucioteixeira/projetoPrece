36ac80126be6f56a82a063a35e986d1342b65143
Glaucio <glaucio.teixeira@outlook.com>
2018-10-23 21:19:18 -0300

    Email nao repetido na insercao do Usuario

--- src/main/java/br/com/cenarioesolucao/projetoPrece/repository/UsuarioRepository.java
+++ src/main/java/br/com/cenarioesolucao/projetoPrece/repository/UsuarioRepository.java
@@ -2,10 +2,19 @@ package br.com.cenarioesolucao.projetoPrece.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.cenarioesolucao.projetoPrece.domain.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
 
+	/**
+	 * Implementa a busca por email 
+	 * @param email
+	 * @return
+	 */
+	
+	@Transactional(readOnly = true)
+	Usuario findByEmail(String email);
 }
--- src/main/java/br/com/cenarioesolucao/projetoPrece/service/validation/UsuarioGuardarValidator.java
+++ src/main/java/br/com/cenarioesolucao/projetoPrece/service/validation/UsuarioGuardarValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.cenarioesolucao.projetoPrece.domain.Usuario;
 import br.com.cenarioesolucao.projetoPrece.domain.enums.TipoUsuario;
 import br.com.cenarioesolucao.projetoPrece.dto.UsuarioNewDTO;
+import br.com.cenarioesolucao.projetoPrece.repository.UsuarioRepository;
 import br.com.cenarioesolucao.projetoPrece.resource.exception.FieldMessage;
 import br.com.cenarioesolucao.projetoPrece.service.validation.utils.BR;
 
 public class UsuarioGuardarValidator implements ConstraintValidator<UsuarioGuardar, UsuarioNewDTO> {
+	@Autowired
+	private UsuarioRepository repository;
+	
 	@Override
 	public void initialize(UsuarioGuardar ann) {
 		
@@ -27,6 +34,12 @@ public class UsuarioGuardarValidator implements ConstraintValidator<UsuarioGuard
 		if (usuarioNewDTO.getTipoUsuario().equals(TipoUsuario.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(usuarioNewDTO.getCpfOuCnpj())) {
 			list.add(new FieldMessage("CpfOuCnpj", "CNPJ Inválido"));
 		}
+		
+		Usuario aux = repository.findByEmail(usuarioNewDTO.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente!"));
+		}
+		
 		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
